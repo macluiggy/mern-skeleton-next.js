429172f02fd2e5b9415ac6eba43984abc8ba3cfe
luiggy macias <ferrinluiggy@gmail.com>
2022-01-26 19:42:13 -0500

    ddd

--- frontend/pages/user/edit/[id].tsx
+++ frontend/pages/user/edit/[id].tsx
@@ -2,18 +2,19 @@ import React from "react";
 import EditProfile from "../../../components/EditProfile";
 import auth from "../../../lib/auth-helper";
 import Redirect from "../../../components/Redirect";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { Container } from "../../../components/Container";
 
 export default function PrivateRoute() {
   const router = useRouter();
   const { id } = router.query;
   return (
-    <Container title="Edit User">
+    <Container title="Redirecting">
       {auth.isAuthenticated() ? (
         <EditProfile userId={id} />
       ) : (
-        <Redirect path="/signin" />
+        // <Redirect path="/signin" />
+        Router.push("/signin")
       )}
     </Container>
   );
