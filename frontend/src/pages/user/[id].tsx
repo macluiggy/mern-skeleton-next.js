4faaee3acbc26f6be5e4274b8c7ceef89a935df1
Luiggy Macias <ferrinluiggy@gmail.com>
2022-01-28 05:21:35 +0000

    ddddd

--- frontend/src/pages/user/[id].tsx
+++ frontend/src/pages/user/[id].tsx
@@ -145,7 +145,7 @@ export async function getStaticPaths() {
     paths: users.map((user: UserProps) => {
       return { params: { id: user._id } };
     }),
-    fallback: false,
+    fallback: "blocking",
   };
 }
 // var jwt = auth.isAuthenticated();
